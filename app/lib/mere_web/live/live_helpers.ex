b1a7d46c7be4ef7892a57d0072f2396fadcfea63
Sam Richardson <sam@richardson.co.nz>
2021-10-12 14:31:19 +1100

    Link to channel

--- app/lib/mere_web/live/live_helpers.ex
+++ app/lib/mere_web/live/live_helpers.ex
@@ -1,27 +1,16 @@
 defmodule MereWeb.LiveHelpers do
   import Phoenix.LiveView.Helpers
 
-  @doc """
-  Renders a component inside the `MereWeb.ModalComponent` component.
-
-  The rendered modal receives a `:return_to` option to properly update
-  the URL when the modal is closed.
-
-  ## Examples
-
-      <%= live_modal MereWeb.YouTubeChannelLive.FormComponent,
-        id: @you_tube_channel.id || :new,
-        action: @live_action,
-        you_tube_channel: @you_tube_channel,
-        return_to: Routes.you_tube_channel_index_path(@socket, :index) %>
-  """
-  def live_modal(component, opts) do
-    path = Keyword.fetch!(opts, :return_to)
-    modal_opts = [id: :modal, return_to: path, component: component, opts: opts]
-    live_component(MereWeb.ModalComponent, modal_opts)
-  end
-
   def time_from_now(time) do
     Mere.Cldr.DateTime.Relative.to_string!(time, relative_to: DateTime.utc_now())
+  end
+
+  def youtube_channel_to_url(youtube_channel) do
+    url =
+      MereWeb.Endpoint.url()
+      |> URI.parse()
+
+    %{url | host: "#{youtube_channel.slug}.#{url.host}"}
+    |> URI.to_string()
   end
 end
